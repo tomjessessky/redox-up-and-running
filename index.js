c739c06742bf80b298056b7fc82d832e1f2173bc
Tom Jessessky <tom@redoxengine.com>
2017-07-26 00:07:24 -0500

    destination

--- index.js
+++ index.js
@@ -18,20 +18,17 @@ app.get('/', function (req, res) {
 app.get('/destination', function (req, res) {
 	if (req.headers['verification-token'] === DESTINATION_VERIFICATION_TOKEN) {
 		console.log('verification-token matched!');
-		res.send(req.query.challenge);
+		return res.send(req.query.challenge);
 	}
 
-	console.log('verification-token did not match :( ');
+	console.log('verfication-token did not match :( ');
 	res.sendStatus(400);
 });
 
 app.post('/destination', function (req, res) {
-	console.log(body);
+	console.log(req.body);
 	res.sendStatus(200);
 });
-
-
-
 
 
 
