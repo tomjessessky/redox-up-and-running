68ae9bb2450e85c19693c724b279f7d4cf818f4c
Tom Jessessky <tom@redoxengine.com>
2017-07-26 00:02:40 -0500

    destination

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 
+var DESTINATION_VERIFICATION_TOKEN = 'abcd1234';
+
 app.use(bodyParser.json());
 
 app.listen(80, function () {
@@ -12,6 +14,23 @@ app.listen(80, function () {
 app.get('/', function (req, res) {
 	res.send('Hello, World!');
 });
+
+app.get('/destination', function (req, res) {
+	if (req.headers['verification-token'] === DESTINATION_VERIFICATION_TOKEN) {
+		console.log('verification-token matched!');
+		res.send(req.query.challenge);
+	}
+
+	console.log('verification-token did not match :( ');
+	res.sendStatus(400);
+});
+
+app.post('/destination', function (req, res) {
+	console.log(body);
+	res.sendStatus(200);
+});
+
+
 
 
 
